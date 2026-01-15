7e22e92cef497a096d55e399a2c5a1e5a0658798
PriyankaJaybhay <jaybhaypriyanka@gmail.com>
2026-01-16 00:55:10 +0100

    fix: Add error handling for invalid book names in API

--- src/main/java/com/bookstore/controller/BasketController.java
+++ src/main/java/com/bookstore/controller/BasketController.java
@@ -5,8 +5,13 @@ import com.bookstore.dto.PriceResponse;
 import com.bookstore.model.Basket;
 import com.bookstore.service.BookPriceCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/basket")
@@ -20,5 +25,13 @@ public class BasketController {
         Basket basket = new Basket(request.getBooks());
         double price = bookPriceCalculatorService.calculatePrice(basket);
         return ResponseEntity.ok(new PriceResponse(price));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidBookName(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Invalid book name, Please provide valid names from the below list");
+        error.put("message", "Valid book codes: CLEAN_CODE, THE_CLEAN_CODER, CLEAN_ARCHITECTURE, TDD_BY_EXAMPLE, LEGACY_CODE");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
