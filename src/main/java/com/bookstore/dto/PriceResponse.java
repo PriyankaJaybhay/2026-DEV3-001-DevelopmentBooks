b1218952fb8c5a936f472145d8e7dc657a7ef2b7
PriyankaJaybhay <jaybhaypriyanka@gmail.com>
2026-01-18 23:25:57 +0100

    Feature : Add EURO as a currency in the response object

--- src/main/java/com/bookstore/dto/PriceResponse.java
+++ src/main/java/com/bookstore/dto/PriceResponse.java
@@ -2,12 +2,14 @@ package com.bookstore.dto;
 
 public class PriceResponse {
     private double totalPrice;
+    private String currency;
 
     public PriceResponse() {
     }
 
     public PriceResponse(double totalPrice) {
         this.totalPrice = totalPrice;
+        this.currency = "EUR";
     }
 
     public double getTotalPrice() {
@@ -16,5 +18,13 @@ public class PriceResponse {
 
     public void setTotalPrice(double totalPrice) {
         this.totalPrice = totalPrice;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 }
